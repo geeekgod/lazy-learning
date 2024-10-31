5e81bd4aa5989dda0c3996608d9e6a6ef583911d
Rishabh Singh <ris8645208@gmail.com>
2024-11-01 00:01:08 +0530

    chore: fix open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,45 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Header } from '@/components/ui/header'
-import { cn } from '@/lib/utils'
-import { ThemeProvider } from '@/components/theme-provider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/ui/header";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Lazy Learning',
-  description: 'Your Personal Learning Assistant',
-  icons: '/logo.png',
-}
+  title: "Lazy Learning",
+  description: "Your Personal Learning Assistant",
+  icons: "/logo.png",
+  openGraph: {
+    title: "Lazy Learning",
+    description: "Your Personal Learning Assistant",
+    images: [
+      {
+        url: "/assets/img/banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Lazy Learning",
+      },
+    ],
+    type: "website",
+    countryName: "India",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={cn(inter.className, "min-h-screen")}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-        >
+        <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
           {children}
         </ThemeProvider>
       </body>
-    </html >
-  )
+    </html>
+  );
 }
